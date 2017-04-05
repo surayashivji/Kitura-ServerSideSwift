fc675d23acfb887f924f3f74878f87bba49b16cd
Suraya Shivji <surayashivji@gmail.com>
2017-04-05 16:24:06 -0700

    instant coder

--- 10instantcoder/Sources/main.swift
+++ 10instantcoder/Sources/main.swift
@@ -1,1 +1,16 @@
-print("Hello, world!")
+import CouchDB
+import Credentials
+import CredentialsGitHub
+import Foundation
+import HeliumLogger
+import Kitura
+import KituraNet
+import KituraSession
+import KituraStencil
+import LoggerAPI
+import SwiftyJSON
+
+HeliumLogger.use()
+
+let router = Router()
+router.setDefault(
