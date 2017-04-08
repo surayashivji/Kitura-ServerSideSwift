e5cebdfc6768cf7c7d9f9977353fce505f882d2b
Suraya Shivji <surayashivji@gmail.com>
2017-04-08 13:11:45 -0700

    10

--- 10instantcoder/Sources/main.swift
+++ 10instantcoder/Sources/main.swift
@@ -10,7 +10,50 @@ import KituraStencil
 import LoggerAPI
 import SwiftyJSON
 
+// decode HTML forms
+extension String {
+    func removeHTMLEncoding() -> String {
+        let result = self.replacingOccurrences(of: "+", with: " ")
+        return result.removingPercentEncoding ?? result
+    }
+}
+
+
+//error handling
+func send(error: String, code: HTTPStatusCode, to response: RouterResponse) {
+    _ = try? response.status(code).send(error).end()
+}
+
+// helper method for getting login form values & checking they were submitted
+// returns finished dictionary
+func getPost(for request: RouterRequest, fields: [String]) -> [String: String]? {
+    
+    // ensure form fields exist
+    guard let values = request.body else { return nil }
+    
+    guard case .urlEncoded(let body) = values else { return nil }
+    
+    var result = [String: String]()
+    
+    for field in fields {
+        if let value = body[field]?.trimmingCharacters(in: .whitespacesAndNewlines) {
+            if value.characters.count > 0 {
+                result[field] = value.removeHTMLEncoding()
+                continue
+            }
+        }
+        return nil
+    }
+    return result
+}
+
+
 HeliumLogger.use()
 
 let router = Router()
-router.setDefault(
+router.setDefault(templateEngine: StencilTemplateEngine())
+router.all("/static", middleware: StaticFileServer())
+router.all(middleware: Session(secret: "Suraya's last name is Shivji"))
+router.post("/", middleware: BodyParser())
+Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.run()
