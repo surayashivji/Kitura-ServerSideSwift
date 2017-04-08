9eb3255c0cb3c6df270a61d8ce189d635fa43582
Suraya Shivji <surayashivji@gmail.com>
2017-04-08 14:51:35 -0700

    github authentication

--- 10instantcoder/Sources/main.swift
+++ 10instantcoder/Sources/main.swift
@@ -54,6 +54,31 @@ let router = Router()
 router.setDefault(templateEngine: StencilTemplateEngine())
 router.all("/static", middleware: StaticFileServer())
 router.all(middleware: Session(secret: "Suraya's last name is Shivji"))
+
+
+
+
 router.post("/", middleware: BodyParser())
+
+// step 1 for auth: create and register github credentials
+let credentials = Credentials()
+let gitCredentials = CredentialsGitHub(clientId: "5b7b077f53376f159460",
+                                       clientSecret: "9c7da9f62decf95d73fc5db18141ffdbe2fea23c",
+                                       callbackUrl: "http://localhost:8090/login/github/callback",
+                                       userAgent: "server-side-swift")
+credentials.register(plugin: gitCredentials)
+
+// step 2 : assign routes to trigger the authentication
+    // one should start authentication process, next should be called by github once user grants approval
+    // if auth fails get redirected back to first route, login to try again
+// tell router that these two routes are handled by configured credentials object ^^
+router.get("/login/github", handler: credentials.authenticate(credentialsType: gitCredentials.name))
+router.get("/login/github/callback", handler: credentials.authenticate(credentialsType: gitCredentials.name))
+credentials.options["failureRedirect"] = "/login/github"
+
+// step 3 - tell router to use credentials object for /projects and /signup route (/signup assumes github auth but no instantcoder login)
+router.all("/projects", middleware: credentials)
+router.all("/signup", middleware: credentials)
+
 Kitura.addHTTPServer(onPort: 8090, with: router)
 Kitura.run()
