472b8513541852e24bfce55c99c3199e7c233c09
Suraya Shivji <surayashivji@gmail.com>
2017-04-15 21:02:19 -0700

    finished project

--- 10instantcoder/Sources/main.swift
+++ 10instantcoder/Sources/main.swift
@@ -58,19 +58,45 @@ func context(for request: RouterRequest) -> [String: Any] {
     result["languages"] = ["C++", "C", "Java", "Swift", "Go", "JavaScript", "Objective-C", "Perl", "Python"]
     return result
 }
-            
-             
+
+//  function call that either returns their full profile or nil if there’s a problem.
+func getUserProfile(for request: RouterRequest, with response: RouterResponse) -> JSON? {
+    // if they haven't authenticated using GitHub, bail out
+    
+    guard let profileID = request.userProfile?.id else {
+        _ = try? response.redirect("/").end() //redirect home
+        return nil
+    }
+    
+    if let _ = request.session? ["gitHubProfile"].dictionaryObject {
+        // they are authenticated and logged in; return their profile
+        return request.session?["gitHubProfile"]
+    } else {
+        // they aren't logged in; see if they have an account
+        database.retrieve(profileID) { user, error in
+            if let _ = error {
+                // user wasn't found – they need to sign up
+                _ = try? response.redirect("/signup").end()
+            } else if let user = user {
+                // user was found, so just log them in
+                request.session?["gitHubProfile"] = user
+            }
+        }
+        // send back their profile
+        return request.session?["gitHubProfile"]
+    }
+    
+}
+
+
 HeliumLogger.use()
 
 let router = Router()
 router.setDefault(templateEngine: StencilTemplateEngine())
 router.all("/static", middleware: StaticFileServer())
-router.all(middleware: Session(secret: "Suraya's last name is Shivji"))
-
-
-
-
+router.all(middleware: Session(secret: "He thrusts his fists against the posts and still insists he sees the ghosts"))
 router.post("/", middleware: BodyParser())
+
 
 // step 1 for auth: create and register github credentials
 let credentials = Credentials()
@@ -79,23 +105,24 @@ let gitCredentials = CredentialsGitHub(clientId: "5b7b077f53376f159460",
                                        callbackUrl: "http://localhost:8090/login/github/callback",
                                        userAgent: "server-side-swift")
 credentials.register(plugin: gitCredentials)
-
-// step 2 : assign routes to trigger the authentication
-    // one should start authentication process, next should be called by github once user grants approval
-    // if auth fails get redirected back to first route, login to try again
-// tell router that these two routes are handled by configured credentials object ^^
-router.get("/login/github", handler: credentials.authenticate(credentialsType: gitCredentials.name))
-router.get("/login/github/callback", handler: credentials.authenticate(credentialsType: gitCredentials.name))
 credentials.options["failureRedirect"] = "/login/github"
 
 // step 3 - tell router to use credentials object for /projects and /signup route (/signup assumes github auth but no instantcoder login)
 router.all("/projects", middleware: credentials)
 router.all("/signup", middleware: credentials)
 
+// step 2 : assign routes to trigger the authentication
+// one should start authentication process, next should be called by github once user grants approval
+// if auth fails get redirected back to first route, login to try again
+// tell router that these two routes are handled by configured credentials object ^^
+router.get("/login/github", handler: credentials.authenticate(credentialsType: gitCredentials.name))
+router.get("/login/github/callback", handler: credentials.authenticate(credentialsType: gitCredentials.name))
+
+
+
 // home page
 router.get("/") {
     request, response, next in
-    
     defer { next() }
     
     var pageContext = context(for: request)
@@ -125,12 +152,12 @@ router.post("/signup") {
     guard let profile = request.userProfile else { return }
     guard let fields = getPost(for: request, fields: ["language"]) else { return }
     
-    // check if the user ID already has an account    
+    // check if the user ID already has an account
     database.retrieve(profile.id) { user, error in
         if let error = error {
             // user wasn't found!
             
-            // fetch their full profile from GitHub          
+            // fetch their full profile from GitHub
             let gitHubURL = URL(string: "http://api.github.com/ user/\(profile.id)")!
             guard var gitHubProfile = try? Data(contentsOf: gitHubURL) else { return }
             
@@ -140,7 +167,7 @@ router.post("/signup") {
             _ = gitHubJSON.dictionaryObject?.removeValue(forKey: "id")
             gitHubJSON["type"].stringValue = "coder"
             gitHubJSON["language"].stringValue = fields["language"]!
-
+            
             database.create(gitHubJSON) { id, rev, doc, error in
                 if let doc = doc {
                     // it worked! Activate their profile
@@ -148,15 +175,146 @@ router.post("/signup") {
                 }
             }
         } else if let user = user {
-            // user was found, so just log them in          
+            // user was found, so just log them in
             request.session?["gitHubProfile"] = user
         }
     }
     
-    // redirect them to the logged-in homepage    
+    // redirect them to the logged-in homepage
     _ = try? response.redirect("/projects/mine").end()
-
+    
 }
+
+router.get("/projects/mine") {
+    request, response, next in
+    defer { next() }
+    
+    // make sure they are fully authenticated and logged in
+    guard let profile = getUserProfile(for: request, with: response) else { return }
+    guard let gitHubID = profile["login"].string else { return }
+    
+    // put together basic page context
+    var pageContext = context(for: request)
+    
+    // attempt to find all our projects
+    
+    database.queryByView("projects_by_owner", ofDesign: "instantcoder", usingParameters: [.keys([gitHubID as Database.KeyType])]) { projects, error in
+        if let error = error {
+            // this shoudlnt happen, but just in case
+            send(error: error.localizedDescription, code: .internalServerError, to: response)       }
+        else if let projects = projects {
+            // store our projects in the context ready for Stencil
+            pageContext["projects"] = projects["rows"].arrayObject
+        }
+    }
+    
+    // active the "My Projects" tab
+    pageContext["page_projects_mine"] = true
+    
+    // render the contet using projects_mine.stencil
+    try response.render("projects_mine", context: pageContext)
+}
+
+router.get("/projects/delete/:id/:rev") {
+    request, response, next in
+    defer { next() }
+    
+    guard let profile = getUserProfile(for: request, with: response) else { return }
+    
+    guard let id = request.parameters["id"] else { return }
+    guard let rev = request.parameters["rev"] else { return }
+    database.delete(id, rev: rev){ error in
+        _ = try? response.redirect("/projects/mine")
+    }
+}
+
+router.get("/projects/new") {
+    request, response, next in
+    defer { next() }
+    guard let profile = getUserProfile(for: request, with: response) else { return }
+    
+    var pageContext = context(for: request)
+    pageContext["page_projects_new"] = true
+    try response.render("projects_new", context: pageContext)
+}
+
+router.post("/projects/new") {    request, response, next in
+    // 1: check we have a fully authenticated user
+    guard let profile = getUserProfile(for: request, with: response) else { return }
+    // 2: make sure all three fields are present
+    guard let fields = getPost(for: request, fields: ["name", "description", "language"]) else {
+        send(error: "Missing required fields", code: .badRequest, to: response)
+        return
+    }
+    // 3: base our new document on their submitted fields
+    var newProject = fields
+    // 4: add "type" and "owner" so we can create CouchDB views, then convert to JSON
+    newProject["type"] = "project"
+    newProject["owner"] = profile["login"].stringValue
+    let newDocument = JSON(newProject)
+    // 5: send the document to CouchDB
+    database.create(newDocument) {  id, revision, doc, error in
+        // 6: show an error or redirect depending on the result
+        if let error = error {
+            send(error: error.localizedDescription, code: .internalServerError, to: response)
+            return
+        }
+        _ = try? response.redirect("/projects/mine")
+        next()
+    }
+}
+
+router.get("/projects/all") {
+    request, response, next in
+    defer { next() }
+    guard let profile = getUserProfile(for: request, with: response) else { return }
+    var pageContext = context(for: request)
+    database.queryByView("projects", ofDesign: "instantcoder", usingParameters: []) { projects, error in
+        if let error = error {
+            send(error: error.localizedDescription, code: .internalServerError, to: response)
+        } else if let projects = projects {
+            pageContext["projects"] = projects["rows"].arrayObject
+        }
+    }
+    pageContext["page_projects_all"] = true
+    try response.render("projects_all", context: pageContext)
+}
+
+router.get("/projects/search") {
+    
+    request, response, next in    defer { next() }
+    // Check we have a fully authenticated user
+    guard let profile = getUserProfile(for: request, with: response) else { return }
+    // set up the basic context for Stencil
+    var pageContext = context(for: request)
+    // if the user specified a search language
+    if let languageParameter = request.queryParameters["language"] {
+        // find all matching projects
+        database.queryByView("projects_by_language", ofDesign: "instantcoder", usingParameters: [.keys([languageParameter as Database.KeyType])]) { projects, error in
+            if let error = error {
+                send(error: error.localizedDescription, code: .internalServerError, to: response)
+            } else if let projects = projects {
+                // add them to our Stencil context
+                pageContext["projects"] = projects["rows"].arrayObject
+            }
+        }
+        // find all matching coders
+        database.queryByView("coders_by_language", ofDesign: "instantcoder", usingParameters: [.keys([languageParameter as Database.KeyType])]) { projects, error in
+            if let error = error {
+                send(error: error.localizedDescription, code: .internalServerError, to: response)
+                
+            } else if let projects = projects {
+                // add them to the Stencil context
+                pageContext["coders"] = projects["rows"].arrayObject
+            }
+        }
+    }
+    // activate the correct tab in master.stencil
+    pageContext["page_projects_search"] = true
+    // render it all
+    try response.render("projects_search", context: pageContext)
+}
+
 
 Kitura.addHTTPServer(onPort: 8090, with: router)
 Kitura.run()
